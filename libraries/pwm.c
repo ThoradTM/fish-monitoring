86d1cdb898f789fad253259f9968ede5fa106179
Thorad <60048256+ThoradTM@users.noreply.github.com>
2025-10-26 18:01:28 -0500

    Wrote PWM library according to knowldege gathered from the datasheet and external sources. 50hz period w/ 1.5ms pulse for off and 1.6ms pulse for on slow

--- libraries/pwm.c
+++ libraries/pwm.c
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
 #include "pwm.h"
+#include "../drivers/gpio.h"
 
 void initPwm()
 {
@@ -17,13 +18,15 @@ void initPwm()
     SYSCTL_RCGCGPIO_R |= (SYSCTL_RCGCGPIO_R2);
     _delay_cycles(3);
 
+
+
 //    GPIO_PORTC_DEN_R |= (32 | 16);
 //    GPIO_PORTC_AFSEL_R |= (32 | 16);
 //    GPIO_PORTB_DEN_R |= (64 | 128);
 //    GPIO_PORTB_AFSEL_R |= (64 | 128);
 
-    GPIO_PORTB_DEN_R |= (128);
-    GPIO_PORTB_AFSEL_R |= (128);
+    GPIO_PORTB_DEN_R |= (64);
+    GPIO_PORTB_AFSEL_R |= (64);
 
 //    GPIO_PORTC_PCTL_R &= ~(GPIO_PCTL_PC5_M | GPIO_PCTL_PC4_M);
 //    GPIO_PORTC_PCTL_R |= (GPIO_PCTL_PC5_M0PWM7 | GPIO_PCTL_PC4_M0PWM6);
@@ -31,69 +34,48 @@ void initPwm()
 //    GPIO_PORTB_PCTL_R &= ~(GPIO_PCTL_PB6_M | GPIO_PCTL_PB7_M);
 //    GPIO_PORTB_PCTL_R |= (GPIO_PCTL_PB6_M0PWM0 | GPIO_PCTL_PB7_M0PWM1);
 
-    GPIO_PORTB_PCTL_R &= ~(GPIO_PCTL_PB7_M);
-    GPIO_PORTB_PCTL_R |= (GPIO_PCTL_PB7_M0PWM1);
+    GPIO_PORTB_PCTL_R &= ~(GPIO_PCTL_PB6_M);
+    GPIO_PORTB_PCTL_R |= (GPIO_PCTL_PB6_M0PWM0);
 
 
     SYSCTL_SRPWM_R = SYSCTL_SRPWM_R0; //Reset pwm0
     SYSCTL_SRPWM_R = 0; // set reset register to 0
 
     PWM0_0_CTL_R = 0;
-    PWM0_3_CTL_R = 0;
+//    PWM0_3_CTL_R = 0;
 
-    PWM0_3_GENB_R = PWM_0_GENB_ACTCMPBD_ONE | PWM_0_GENB_ACTLOAD_ZERO;
-    PWM0_3_GENA_R = PWM_0_GENA_ACTCMPAD_ONE | PWM_0_GENA_ACTLOAD_ZERO;
+    // PWM0_3_GENB_R = PWM_0_GENB_ACTCMPBD_ONE | PWM_0_GENB_ACTLOAD_ZERO;
+    // PWM0_3_GENA_R = PWM_0_GENA_ACTCMPAD_ONE | PWM_0_GENA_ACTLOAD_ZERO;
 
-    PWM0_0_GENB_R = PWM_0_GENB_ACTCMPBD_ONE | PWM_0_GENB_ACTLOAD_ZERO;
+    // PWM0_0_GENB_R = PWM_0_GENB_ACTCMPBD_ONE | PWM_0_GENB_ACTLOAD_ZERO;
     PWM0_0_GENA_R = PWM_0_GENA_ACTCMPAD_ONE | PWM_0_GENA_ACTLOAD_ZERO;
 
-    PWM0_0_LOAD_R = 1024;
-    PWM0_3_LOAD_R = 1024;
+    PWM0_0_LOAD_R = 800000; // 50 hz period
+    //PWM0_3_LOAD_R = 1024;
 
 
-    PWM0_0_CMPB_R = 0;
+    //PWM0_0_CMPB_R = 0;
     PWM0_0_CMPA_R = 0;
-    PWM0_3_CMPB_R = 0;
-    PWM0_3_CMPA_R = 0;
+    // PWM0_3_CMPB_R = 0;
+    // PWM0_3_CMPA_R = 0;
 
-    PWM0_3_CTL_R = PWM_0_CTL_ENABLE;
+    //PWM0_3_CTL_R = PWM_0_CTL_ENABLE;
     PWM0_0_CTL_R = PWM_0_CTL_ENABLE;
 
-    PWM0_ENABLE_R |= (PWM_ENABLE_PWM0EN | PWM_ENABLE_PWM1EN | PWM_ENABLE_PWM6EN | PWM_ENABLE_PWM7EN);
+    PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN //| PWM_ENABLE_PWM1EN | PWM_ENABLE_PWM6EN | PWM_ENABLE_PWM7EN);
 }
 
-void motorSpeedFw(int ms0){ // Forward a constant speed
-    desired_pwm = ms0;
-    PWM0_0_CMPA_R = ms0;
-    PWM0_3_CMPB_R = ms0 + sf;
-}
+// 64000 for slow
 
-void motorSpeedBw(int ms0){ // Backwards a constant speed
-    desired_pwm = ms0;
-    PWM0_0_CMPB_R = ms0;
-    PWM0_3_CMPA_R = ms0 + sf;
-}
+// 60000 for off
 
-void motorSpeedCcw(int ms0){ // Clockwise
-    desired_pwm = ms0;
-    PWM0_0_CMPB_R = ms0;
-    PWM0_3_CMPB_R = ms0 + sf;
-}
-
-void motorSpeedCw(int ms0){ // Counterclockwise
-    desired_pwm = ms0;
-    PWM0_0_CMPA_R = ms0;
-    PWM0_3_CMPA_R = ms0 + sf;
+void servoSlow(){ // Forward a constant speed
+    PWM0_0_CMPA_R = 64000;
 }
 
 
-
-void motorSpeedZero(){ // Zero All Motor Speeds
-    desired_pwm = 0;
-    PWM0_0_CMPB_R = 0;
-    PWM0_0_CMPA_R = 0;
-    PWM0_3_CMPB_R = 0;
-    PWM0_3_CMPA_R = 0;
+void servoStop(){ // Zero All Motor Speeds
+    PWM0_0_CMPA_R = 60000;
 }
 
 
