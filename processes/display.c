0a1e1336ae56f6844635ab602c87ba84b3d4af38
Thorad <60048256+ThoradTM@users.noreply.github.com>
2025-10-26 19:25:04 -0500

    Added basic functionality to display.c

--- processes/display.c
+++ processes/display.c
@@ -4,6 +4,7 @@
 #include "../kernel/kernel.h"
 #include "../kernel/servicecalls.h"
 #include "../drivers/uart0.h"
+#include "../libraries/pwm.h"
 
 
 
@@ -15,6 +16,27 @@ void initDisplay(DisplayContext * myDisplay)
 	putsGraphicsLcd(myDisplay, "Display Ready");  // Print "Display Ready" on the LCD
 }
 
+enum states
+{
+	DISPLAY,
+	SETTINGS,
+	SCREENSAVER
+};
+
+void consumerStateMachine()
+{
+	states state = DISPLAY;
+	switch(state)
+	{
+		case DISPLAY: 
+			displayTask();
+			break;
+		
+		case SETTINGS:
+
+
+	}
+}
 
 void displayTask()
 {
@@ -41,7 +63,6 @@ void displayTask()
 		putiUart0(shmHandle->turbidity);
 		putcUart0('\n');
 
-
 		//drawGraphicsLcdRectangle(&myDisplay, 10, 10, 50, 20, INVERT); // Draw a rectangle on the display
 //        setGraphic();
 //		setGraphicsLcdTextPosition(&myDisplay, 20, 2);                 // Set text position
@@ -54,5 +75,59 @@ void displayTask()
 //        putiGraphicsLcd(lcdHandler, i);
 //        sleep(1000);
 //        clearGraphicsLcd(lcdHandler);
+	}
+}
+
+void settingsTask()
+{
+    DisplayContext myDisplay;
+    DisplayContext * lcdHandler = &myDisplay;
+    initDisplay(lcdHandler);
+
+	shmPerms();
+    shm * shmHandle = getShmHandle();
+
+	//uint32_t i = 0;
+
+    while (1)
+	{
+
+		sleep(1000);
+
+
+		putsUart0("Temperature: ");
+		putiUart0(shmHandle->temperature);
+		putcUart0('\n');
+
+		putsUart0("Turbidity: ");
+		putiUart0(shmHandle->turbidity);
+		putcUart0('\n');
+	}
+}
+
+void screensaverTask()
+{
+    DisplayContext myDisplay;
+    DisplayContext * lcdHandler = &myDisplay;
+    initDisplay(lcdHandler);
+
+	shmPerms();
+    shm * shmHandle = getShmHandle();
+
+	//uint32_t i = 0;
+
+    while (1)
+	{
+
+		sleep(1000);
+
+
+		putsUart0("Temperature: ");
+		putiUart0(shmHandle->temperature);
+		putcUart0('\n');
+
+		putsUart0("Turbidity: ");
+		putiUart0(shmHandle->turbidity);
+		putcUart0('\n');
 	}
 }
