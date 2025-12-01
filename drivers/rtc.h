a74fbf8f8eb60eb7b930e06d13aec8980fe5c29f
Thorad <60048256+ThoradTM@users.noreply.github.com>
2025-12-01 00:12:51 -0600

    comment fixes

--- drivers/rtc.h
+++ drivers/rtc.h
@@ -11,7 +11,7 @@ typedef struct {
     uint8_t hours;      // 0-23
     uint8_t day;        // 1-31
     uint8_t month;      // 1-12
-    uint16_t year;      // Full year (e.g., 2025)
+    uint16_t year;      // Year
 } RTCDateTime;
 
 void RTCInit(void);
