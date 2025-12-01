f67db3d038f864f4369fdb3abd9ca6290f23736a
Thorad <60048256+ThoradTM@users.noreply.github.com>
2025-11-30 20:39:00 -0600

    Formatting fixes

--- drivers/rtc.h
+++ drivers/rtc.h
@@ -1,14 +1,10 @@
-// RTC EXAMPLE. FOR REFERENCE USE ONLY.
-
-
-/* rtc.h - RTC Driver Header for TM4C123GH6PM */
 #ifndef RTC_H
 #define RTC_H
 
 #include <stdint.h>
 #include <stdbool.h>
 
-/* RTC Date/Time Structure */
+
 typedef struct {
     uint8_t seconds;    // 0-59
     uint8_t minutes;    // 0-59
@@ -18,12 +14,11 @@ typedef struct {
     uint16_t year;      // Full year (e.g., 2025)
 } RTCDateTime;
 
-/* Function Prototypes */
 void RTCInit(void);
 void RTCSetDateTime(const RTCDateTime *dt);
 void RTCGetDateTime(RTCDateTime *dt);
 
 uint32_t RTCGetSeconds(void);
 
-#endif /* RTC_H */
+#endif
 
